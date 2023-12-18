d9de76f41f8fc94d683a9c80bbc0b6a52984ba35
Piyush <piyushbansal941@gmail.com>
2023-12-18 23:24:02 +0530

    ui fixes

--- src/components/subcomponents/DragCategoryOptionMapping.js
+++ src/components/subcomponents/DragCategoryOptionMapping.js
@@ -45,12 +45,12 @@ function DragCategoryOptionMapping(props) {
         <>
             <div  className={` ${isOver ? 'bg-pink-100' : ''} p-2`} ref={drop}>
                 <div className={`flex gap-2 ${isDragging ? 'bg-slate-100' : ''}`} ref={drag}>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="mt-1 bi bi-filter-circle" viewBox="0 0 16 16">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="mt-1 mr-1 bi bi-filter-circle shrink-0" viewBox="0 0 16 16">
                         <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16" />
                         <path d="M7 11.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 0 1h-1a.5.5 0 0 1-.5-.5m-2-3a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5m-2-3a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5" />
                     </svg>
 
-                    <div className="flex items-center gap-5">
+                    <div className="flex items-center gap-2">
 
                         <Input type="text" name="option" placeholder="Option" value={props.ele} onchange={(e) => props.handleOptionChange(props.currentElementIndex, e.target.value)} />
 
--- src/components/subcomponents/Input.js
+++ src/components/subcomponents/Input.js
@@ -4,7 +4,7 @@ export default function Input(props) {
     return (
         <>
             <div className='mb-2 mr-1'>
-                <input ref={props.eleRef} type={props.type} className='mb-1 shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight  focus:shadow-outline focus:outline-blue-400' name={props.name} id={props.id} placeholder={props.placeholder} value={props.value} onChange={props.onchange}  />
+                <input ref={props.eleRef} type={props.type} className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight  focus:shadow-outline focus:outline-blue-400' name={props.name} id={props.id} placeholder={props.placeholder} value={props.value} onChange={props.onchange}  />
             </div>
         </>
     )
